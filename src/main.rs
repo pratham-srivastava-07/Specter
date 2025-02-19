1d38612f8035dcede3a792887d9d0094681eeb32
Krish <prathamrajsri2003@gmail.com>
2025-02-19 23:08:34 +0530

    socket version && auth method

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // accepting an incoming client request
         let (mut socket,  add) = listener.accept().await?;
         println!("new connection from client {}", add);
-         // Read some data (Just for testing)
+         // Read some data 
          let mut buffer = [0; 1024];
          let n = socket.read(&mut buffer).await?;
          println!("Received {} bytes: {:?}", n, &buffer[..n]);
+         println!("{}\n{}", &buffer[0], &buffer[1]);
+         
+         
  
-         // Echo the data back (Temporary, we will replace this with real proxy logic)
+         // Echo the data back 
          socket.write_all(&buffer[..n]).await?;
     }
 }
+
+// 
 
