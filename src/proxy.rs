c2d0d34a3f4f5c4549ee1a4cbf970c65d64b3ba4
Krish <prathamrajsri2003@gmail.com>
2025-03-12 00:41:54 +0530

    fix: auth

--- src/proxy.rs
+++ src/proxy.rs
@@ -8,14 +8,14 @@ use crate::auth;
 pub async fn handle_client(mut socket: TcpStream) -> Result<(), Box<dyn Error>> {
     let request = TcpRequest::request_client(&mut socket).await?;
     let blocked_domains = ["github.com"];
-    let headers = TcpRequest::request_client(&mut socket).await?.headers;
+    // let headers = TcpRequest::request_client(&mut socket).await?.headers;
 
     if blocked_domains.contains(&request.dest.as_str()) {
         warn!("Cannot forward request to {}", request.dest);
         return Ok(());
     }
 
-    let token = auth::extract_auth_token(&headers);
+    let token = auth::extract_auth_token(&request.headers);
 
     if let Some(token) = token {
         info!("Authorization Token {}", token);
@@ -33,13 +33,13 @@ pub async fn handle_client(mut socket: TcpStream) -> Result<(), Box<dyn Error>> 
     info!("Connected to target server: {}:{}", request.dest, request.port);
 
     // Start forwarding data
-    let (mut client_reader, mut client_writer) = socket.split();
-    let (mut server_reader, mut server_writer) = target_stream.split();
+    // let (mut client_reader, mut client_writer) = socket.split();
+    // let (mut server_reader, mut server_writer) = target_stream.split();
 
-    let client_to_server = tokio::io::copy(&mut client_reader, &mut server_writer);
-    let server_to_client = tokio::io::copy(&mut server_reader, &mut client_writer);
+    // let client_to_server = tokio::io::copy(&mut client_reader, &mut server_writer);
+    // let server_to_client = tokio::io::copy(&mut server_reader, &mut client_writer);
 
-    tokio::try_join!(client_to_server, server_to_client)?;
+    tokio::io::copy_bidirectional(&mut socket, &mut target_stream).await?; // pipes raw bites without interpretting them 
 
     Ok(())
 }
